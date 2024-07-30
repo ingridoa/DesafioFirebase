6009f0ab0ff5eca7a5ed24fa3e111555b5c5417c
Ingrid <ingrid.oa@gmail.com>
2024-07-29 21:33:18 -0400

    desafioFirebase

---
+++ src/config/FirebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyfGNhlwmhHildOsu-USMkpEfJsP1u9Js",
+  authDomain: "crud-colaboradores-ed35a.firebaseapp.com",
+  projectId: "crud-colaboradores-ed35a",
+  storageBucket: "crud-colaboradores-ed35a.appspot.com",
+  messagingSenderId: "143737435077",
+  appId: "1:143737435077:web:146cbf44b15233d68351ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,88 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form @submit.prevent="addColaborador">
+      <input v-model="nuevoColaborador.nombre" placeholder="Ingrese Nombre" />
+      <input v-model="nuevoColaborador.correo" placeholder="Ingrese Correo" />
+      <button type="submit">Agregar</button>
+    </form>
+    <ul>
+      <li v-for="colaborador in colaboradores" :key="colaborador.id">
+        {{ colaborador.id }} - {{ colaborador.nombre }} - {{ colaborador.correo }}
+        <button @click="deleteColaborador(colaborador.id)">Eliminar</button>
+        <button @click="startEditColaborador(colaborador)">Editar</button>
+      </li>
+    </ul>
+
+    <div v-if="isEditing">
+      <h3>Editar Colaborador</h3>
+      <form @submit.prevent="editColaborador">
+        <input v-model="colaboradorToEdit.nombre" placeholder="Ingrese Nombre" />
+        <input v-model="colaboradorToEdit.correo" placeholder="Ingrese Correo" />
+        <button type="submit">Guardar Cambios</button>
+        <button type="button" @click="cancelEdit">Cancelar</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { getFirestore, collection, onSnapshot, addDoc, doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import App from '../config/FirebaseConfig';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      nuevoColaborador: { nombre: '', correo: '' },
+      colaboradores: [],
+      isEditing: false,
+      colaboradorToEdit: null
+    };
+  },
+  mounted() {
+    const db = getFirestore(App);
+    const colaboradoresRef = collection(db, 'colaboradores');
+    onSnapshot(colaboradoresRef, (snapshot) => {
+      this.colaboradores = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    });
+  },
+  methods: {
+    async addColaborador() {
+      if (this.nuevoColaborador.nombre.trim() === '' || this.nuevoColaborador.correo.trim() === '') return;
+      const db = getFirestore(App);
+      const colaboradoresRef = collection(db, 'colaboradores');
+      await addDoc(colaboradoresRef, { nombre: this.nuevoColaborador.nombre, correo: this.nuevoColaborador.correo });
+      this.nuevoColaborador = { nombre: '', correo: '' }; // Limpiar campos después de agregar
+    },
+    async deleteColaborador(colaboradorId) {
+      const db = getFirestore(App);
+      const colaboradorRef = doc(db, 'colaboradores', colaboradorId);
+      await deleteDoc(colaboradorRef);
+    },
+    startEditColaborador(colaborador) {
+      this.colaboradorToEdit = { ...colaborador };
+      this.isEditing = true;
+    },
+    async editColaborador() {
+      const db = getFirestore(App);
+      const colaboradorRef = doc(db, 'colaboradores', this.colaboradorToEdit.id);
+      await updateDoc(colaboradorRef, {
+        nombre: this.colaboradorToEdit.nombre,
+        correo: this.colaboradorToEdit.correo
+      });
+      this.cancelEdit();
+    },
+    cancelEdit() {
+      this.isEditing = false;
+      this.colaboradorToEdit = null;
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+img {
+  height: 150px;
+  width: 200px;
+  object-fit: contain;
+}
+</style>
